3343a5a5692c44803186bc20d59afaf44f7c2a95
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2023-09-04 10:47:39 +0300

    lab01 was added

---
+++ Labs/Lab01/task2.js
@@ -0,0 +1,3 @@
+var name = "Джон";
+var admin = name;
+alert(admin);
---
+++ Labs/Lab01/task4.js
@@ -0,0 +1,5 @@
+var planetName = "Земля";
+var visitorName = "АндрюхаТанкист2013";
+
+document.getElementById("planetName").textContent = planetName;
+document.getElementById("visitorName").textContent = visitorName;
